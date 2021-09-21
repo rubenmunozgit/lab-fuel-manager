c64a9cde14914110aba8cf72b10f95dea59a575c
rpalomino <rpalomino@vrbo.com>
2021-09-21 17:51:55 +0200

    change auth context add useEffect

--- src/universal/contexts/authContext/AuthProvider.js
+++ src/universal/contexts/authContext/AuthProvider.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import AuthContext from './AuthContext';
 import {authApp} from '../../../firebase-client';
 
@@ -7,9 +7,22 @@ const isAuthenticated = (user) => {
 }
 
 const AuthProvider = ({children, isSSRAuth}) => {
-  const [user, setUser] = useState(isSSRAuth);
+  const [isAuth, setIsAuth] = useState(isSSRAuth);
+  const [token, setToken] = useState('');
 
-  authApp.onAuthStateChanged(authApp.auth, (user) => setUser(user));
+  useEffect(() => {
+    const unsubscribe = authApp.onAuthStateChanged(authApp.auth, (user) => {
+      if (user) {
+        setIsAuth(true);
+        setToken(user.accessToken);
+      }
+      else {
+        setIsAuth(false);
+        setToken('');
+      }
+    });
+    return () => unsubscribe();
+  },[]);
 
   const emailPassSignIn = (email, password) => {
     return authApp.signInWithEmailAndPassword(authApp.auth, email, password);
@@ -20,8 +33,8 @@ const AuthProvider = ({children, isSSRAuth}) => {
   };
 
   const context = {
-    token: isAuthenticated(user) ? user.accessToken : false,
-    isAuth: isAuthenticated(user),
+    token,
+    isAuth,
     emailPassSignIn,
     logOut,
     google: ''
